afb6a201421d5a42dd69c5e2cac16f17dafed468
Lukas <rsslks@gmail.com>
2024-07-19 09:18:27 +0200

    Added Errorhandling with additional styling options

--- app/__init__.py
+++ app/__init__.py
@@ -35,6 +35,9 @@ def create_app(config_class=Config):
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
     
+    from app.error import bp as error_bp
+    app.register_blueprint(error_bp)
+    
     
     if not app.debug and not app.testing:
         if not os.path.exists("logs"):
